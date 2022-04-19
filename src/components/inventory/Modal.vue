2590d0c7429446641311ece26b689c31183e47de
JC <ms0242808@gmail.com>
2022-04-19 10:46:57 +0100

    inventory v1.1

---
+++ src/components/inventory/Modal.vue
@@ -0,0 +1,142 @@
+<template>
+  <b-modal ref="addModal" id="addModal" :title="$t('inv.add')" @hide="hideModal('addModal')" centered> 
+    <b-row>
+      <b-col cols="12" class="mt-2">
+        <a class="space">{{$t('inv.category')}}</a>
+        <b-form-input v-model="catVal" list="cat-list" :placeholder="$t('inv.catInput')"></b-form-input>
+        <datalist id="cat-list">
+          <option v-for="cat in catList" :key="cat">{{ cat }}</option>
+        </datalist>
+      </b-col>
+      <b-col cols="12" class="mt-2">
+        <a class="space">{{$t('inv.company')}}</a>
+        <b-form-input v-model="companyVal" list="company-list" :placeholder="$t('inv.companyInput')"></b-form-input>
+        <datalist id="company-list">
+          <option v-for="co in companyList" :key="co">{{ co }}</option>
+        </datalist>
+      </b-col>
+      <b-col cols="12" class="mt-2">
+        <a class="space">{{$t('inv.item')}}</a>
+        <b-form-input v-model="itemVal" id="invItem" type="text"></b-form-input>
+      </b-col>
+      <b-col cols="6" class="mt-2">
+        <a class="space">{{$t('inv.date')}}</a>
+        <b-form-input v-model="dateVal" id="invDate" type="date"></b-form-input>
+      </b-col>
+      <b-col cols="6" class="mt-2">
+        <a class="space">{{$t('inv.time')}}</a>
+        <b-form-input v-model="timeVal" id="invTime" type="time"></b-form-input>
+      </b-col>
+      <b-col cols="4" class="mt-2">
+        <a class="space">{{$t('inv.quantity')}}</a>
+        <b-form-input v-model="quantityVal" id="invQuantity" type="number"></b-form-input>
+      </b-col>
+      <b-col cols="4" class="mt-2">
+        <a class="space">{{$t('inv.unit')}}</a>
+        <b-form-input v-model="unitVal" list="unit-list" :placeholder="$t('inv.unitInput')"></b-form-input>
+        <datalist id="unit-list">
+          <option v-for="un in unitList" :key="un">{{ un }}</option>
+        </datalist>
+      </b-col>
+      <b-col cols="4" class="mt-2">
+        <a class="space">{{$t('inv.price')}}</a>
+        <b-form-input v-model="priceVal" id="invPrice" type="number"></b-form-input>
+      </b-col>
+    </b-row>
+    <template #modal-footer>
+      <div class="w-100">
+        <b-button variant="outline-success" class="float-right ml-1 text-14" @click="addItem()" :disabled="btnState == 1">{{$t('inv.add')}}</b-button>
+        <b-button variant="outline-dark text-14" class="float-right" @click="hideModal('addModal')">{{$t('inv.cancel')}}</b-button>
+      </div>
+    </template>
+  </b-modal>
+</template>
+<script>
+import store from '../../store/store'
+import { functions, httpsCallable, arrayUnion, db, doc, setDoc, updateDoc, collection, query, where, onSnapshot, auth, updatePassword,EmailAuthProvider,reauthenticateWithCredential, perf, trace} from "../../fire"
+
+export default {
+  name:'modal',
+  data(){
+    return{
+      brandL: store.getters.getLocation,
+      catVal:'',
+      itemVal:'',
+      companyVal:'',
+      dateVal:'',
+      timeVal:'',
+      quantityVal:'',
+      unitVal:'',
+      priceVal:'',
+    }
+  },
+  props:{
+    catList: {type: Array},
+    companyList: {type: Array},
+    unitList: {type: Array}
+  },
+  computed:{
+    'btnState':function(){
+      if(this.catVal.length > 0 && this.itemVal.length > 0 && this.companyVal.length > 0 && this.dateVal.length > 0 && this.timeVal.length > 0 && this.quantityVal.length > 0 && this.unitVal.length > 0 && this.priceVal.length > 0){return 0}
+      else{return 1}
+    }
+  },
+  methods:{
+    hideModal(id){
+      this.$bvModal.hide(id);
+      this.catVal='';
+      this.itemVal='';
+      this.companyVal='';
+      this.dateVal='';
+      this.timeVal='';
+      this.quantityVal='';
+      this.unitVal='';
+      this.priceVal='';
+    },
+    async addItem(){
+      const t = trace(perf,"addInventory");
+			t.start();
+      var x = this.dateVal.split('-'),
+      y = this.timeVal.split(':');
+      const newItem = doc(collection(db, "inventory",this.brandL,x[0]+x[1]+x[2]));
+      await setDoc(newItem, {
+        category: this.catVal,
+        item: this.itemVal,
+        company: this.companyVal,
+        date: this.dateVal,
+        time: this.timeVal,
+        quantity: this.quantityVal,
+        unit: this.unitVal,
+        price: this.priceVal,
+        added: x[0]+x[1]+x[2]+y[0]+y[1]
+      });
+      if(!this.catList.includes(this.catVal)){
+        const newCat = doc(db, "inventory", this.brandL);
+        await updateDoc(newCat,{category: arrayUnion(this.catVal)});
+      }
+      if(!this.companyList.includes(this.companyVal)){
+        const newCompany = doc(db, "inventory", this.brandL);
+        await updateDoc(newCompany,{company: arrayUnion(this.companyVal)});
+      }
+      if(!this.unitList.includes(this.unitVal)){
+        const newUnit = doc(db, "inventory", this.brandL);
+        await updateDoc(newUnit,{unit: arrayUnion(this.unitVal)});
+      }
+      this.hideModal('addModal');
+      t.stop();
+    }
+  }
+}
+</script>
+
+<style scoped>
+.spcae{
+  white-space: pre-line;
+}
+a{
+  color:initial;
+}
+.text-14{
+  font-size: 14px;
+}
+</style>
--- src/views/Inventory.vue
+++ src/views/Inventory.vue
@@ -4,33 +4,28 @@
     <div class="mt-3">
       <b-row>
         <b-col sm="12" lg="6">
-          <b-button-group>
-            <b-button variant="success">Add</b-button>
-            <b-button variant="warning">Delete</b-button>
-          </b-button-group>
+          <b-button size="sm" class="mx-1" variant="success" v-b-modal="'addModal'"><font-awesome-icon fixed-width icon="plus"/>{{$t('inv.add')}}</b-button>
+          <b-button size="sm" class="mx-1" variant="outline-danger"><font-awesome-icon fixed-width icon="trash-alt"/>{{$t('inv.delete')}}</b-button>
+          <!-- <b-button size="sm" class="mx-1" variant="outline-dark"><font-awesome-icon fixed-width icon="cog"/>{{$t('inv.setting')}}</b-button> -->
+          <!-- <b-button size="sm" class="mx-1" variant="outline-primary"><font-awesome-icon fixed-width icon="file-download"/>{{$t('inv.export')}}</b-button> -->
         </b-col>
         <b-col sm="12" lg="6">
-          <b-form-group label="Filter" label-for="filter-input" label-cols-sm="3" label-align-sm="right" label-size="sm" class="mb-0">
+          <b-form-group label-cols-sm="3" label-align-sm="right" label-size="sm" class="mb-0">
             <b-input-group size="sm">
-              <b-form-input id="filter-input" v-model="filter" type="search" placeholder="Type to Search"></b-form-input>
+              <b-form-input id="filter-input" v-model="filter" type="search" :placeholder="$t('inv.search')"></b-form-input>
             </b-input-group>
           </b-form-group>
-          <b-button variant="success">Export</b-button>
         </b-col>
       </b-row>
-      <b-table striped hover responsive :items="items" :fields="fields" :filter="filter">
+      <b-table striped hover responsive :items="invList" :fields="fields" :filter="filter" class="mt-3">
 
       </b-table>
     </div>
+    <Modal :catList="catList" :companyList="companyList" :unitList="unitList"/>
   </div>
   <!--
     https://www.mimo.work/help/category/consumables
     https://www.behance.net/gallery/55390583/Inventory-management-dashboard
-    display by table, like permission page
-    新增耗材分類 -> modal with text input & submit button
-    新增耗材(名稱) -> modal with text inputs (brand and product name) & submit button
-    添加庫存 -> modal with select input, text input, date, time & save button
-    剩餘庫存, 警示 -> display on card
     刪除耗材或分類 -> modal with warning text & delete button
     變更耗材分類名稱或耗材名稱 -> modal with text input & update button
   -->
@@ -39,13 +34,22 @@
 <script>
 import store from '../store/store'
 import { mapGetters } from 'vuex'
+import Modal from '../components/inventory/Modal.vue'
+import { functions, httpsCallable, db, doc, setDoc, updateDoc, collection, query, where, onSnapshot, auth, updatePassword,EmailAuthProvider,reauthenticateWithCredential, perf, trace} from "../fire"
 
 export default {
   name: 'Inventory',
+  components:{
+    Modal
+  },
   data(){
     return{
-      items:[],
-      filter: null
+      brandL: store.getters.getLocation,
+      invList:[],
+      filter: null,
+      catList:[],
+      companyList:[],
+      unitList:[]
     }
   },
   computed:{
@@ -55,18 +59,34 @@ export default {
     ]),
     'fields':function(){
       return [
-        // {"key":"Category",label:this.$i18n.t('inv.name')},
-        // {"key":"Name",label:this.$i18n.t('inv.name')},
-        // {"key":"Available",label:this.$i18n.t('inv.role')},
-        // {"key":"Used",label:this.$i18n.t('inv.locations')},
-        // {"key":"Comapny name",label:this.$i18n.t('inv.action')}
-        // {"key":"Last added",label:this.$i18n.t('inv.action')}
-        // {"key":"Action",label:this.$i18n.t('inv.action')}
+        {"key":"Category",label:this.$i18n.t('inv.category')},
+        {"key":"Name",label:this.$i18n.t('inv.name')},
+        {"key":"Available",label:this.$i18n.t('inv.available')},
+        {"key":"Used",label:this.$i18n.t('inv.used')},
+        {"key":"Comapny name",label:this.$i18n.t('inv.company')},
+        {"key":"Last added",label:this.$i18n.t('inv.last_added')},
+        {"key":"Action",label:this.$i18n.t('inv.action')}
       ]
     }
   },
   methods:{
+    async getinputList(){
+      const t = trace(perf,"getInputList");
+			t.start();
+      const unsubscribe = onSnapshot(doc(db, "inventory", this.brandL), (doc) => {
+        this.catList = doc.data()['category'];
+        this.companyList = doc.data()['company'];
+        this.unitList = doc.data()['unit'];
+      });
+      t.stop();
+    },
+    async getCatTable(){
+      // const t = trace(perf,"getCatTable");
+			// t.start();
+      // list out all find same category && item & company
 
+      // t.stop();
+    }
   },
   watch:{
     getLocation(val){
@@ -75,7 +95,8 @@ export default {
     }
   },
   mounted(){
-    // this.getStaff();
+    this.getinputList();
+    this.getCatTable();
     // if(this.brandL == '-'){
     //   this.brandL = this.getLocation;
     //   this.getStaff();
